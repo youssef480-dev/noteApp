bbb9694a4f4e32dc99ed38b4f2165b8ac24ad6c1
youssef <yo8730162@gmail.com>
2025-10-01 18:03:00 +0300

    setup firebase in the project

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,22 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:note_application/firebase_options.dart';
 import 'package:note_application/screen/screen.dart';
 import 'package:note_application/helpers/hive_helpers.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 
 void main() async { 
+
+
    WidgetsFlutterBinding.ensureInitialized();
   await Hive.initFlutter();
   await Hive.openBox(HiveHelpers.notesBox);
   await HiveHelpers.getNotes();
     runApp(MyApp());
+    
+await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+);
 
 
 }
